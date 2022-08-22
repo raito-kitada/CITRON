a4e30dcf33bd8851b19016c3ffb6cc4ae3333015
raito-kitada <k.raito@librus.jp>
2022-08-23 06:37:31 +0900

    update:ranking scroll

--- assets/ranking.js
+++ assets/ranking.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
     $(window).resize( () => {
+        var ref_width=$('.brand').find('.box-item').width();
+        console.log(ref_width);
+        $('.ranking').find('.box-item').css("width",ref_width);
+        var ref_width=$('.brand').find('.box-row').width();
+        $('.ranking').find('.box-row').css("width",ref_width*2.5);
     });
-
 });
